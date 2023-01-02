a20d85bc31148dbc546741d48b7775e964827c9b
Teo Ghidersa <teos.ghidersa@outlook.de>
2023-01-02 21:15:27 +0100

    I first coded a barplot (with labels), then modified it to a facette of subplots for better visualization. This should do it - for now.

--- R/Plots.R
+++ R/Plots.R
@@ -6,5 +6,20 @@
 ##----Unfinished: needs some work on labels-------
 
 Plot1 <- ggplot2::ggplot(data = accidents21)+
-  ggplot2::geom_bar(mapping = aes(x = day_of_week, fill = accident_severity), position = "dodge")
+  ggplot2::geom_bar(mapping = aes(x = day_of_week, fill = accident_severity), position = "dodge") +
+  ggplot2::labs(title = "Total amount of traffic accidents in 2021 (UK)",
+                subtitle = "In progress...",
+                caption = "published by the government of the United Kingdom")+
+  scale_fill_brewer(name = "Accident Severity", palette = "Set1")+
+  scale_y_continuous(name ="Amount of accidents")+
+  scale_x_discrete(name = "Day of week")
+
+
+
+
+Subplot1 <- Plot1 + facet_wrap(~accident_severity, scales = "free")
+
+Subplot1
+
+
 
