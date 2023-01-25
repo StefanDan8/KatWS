1b76cff12c1d4a538323d2a01eeeb5bda809e597
Teo Ghidersa <teos.ghidersa@outlook.de>
2023-01-25 20:19:13 +0100

    updated piechart

--- R/Plots.R
+++ R/Plots.R
@@ -1,8 +1,5 @@
 #In this script there will be plots
 
-library("tidyverse")
-
-load("~/katws/data/accidents21.rda")
 accidents21$day_of_week <- factor(
   accidents21$day_of_week,
   levels = c("Monday","Tuesday" , "Wednesday", "Thursday", "Friday" , "Saturday", "Sunday")
@@ -12,24 +9,19 @@ accidents21$day_of_week <- factor(
 
 
 
-##First plot will showcase the total amount of accidents in GB in the year 2021
 
-#Furthermore, it also shows how high the amount of accident severity was registered:
+#Plot1 <- ggplot2::ggplot(data = accidents21)+
+ # ggplot2::geom_bar(mapping = aes(x = day_of_week, fill = accident_severity), position = "dodge") +
+  #ggplot2::labs(title = "Total amount of traffic accidents in 2021 (UK)",
+#                subtitle = "Accident severity included on the plot",
+ #               caption = "published by the government of the United Kingdom")+
+  #scale_fill_brewer(name = "Accident severity", palette = "Set1")+
+  #scale_y_continuous(name ="Amount of accidents")+
+  #scale_x_discrete(name = "Day of week")
 
-##----Unfinished: needs some work on labels-------
-
-Plot1 <- ggplot2::ggplot(data = accidents21)+
-  ggplot2::geom_bar(mapping = aes(x = day_of_week, fill = accident_severity), position = "dodge") +
-  ggplot2::labs(title = "Total amount of traffic accidents in 2021 (UK)",
-                subtitle = "Accident severity included on the plot",
-                caption = "published by the government of the United Kingdom")+
-  scale_fill_brewer(name = "Accident severity", palette = "Set1")+
-  scale_y_continuous(name ="Amount of accidents")+
-  scale_x_discrete(name = "Day of week")
-
-Plot1
-Subplot1 <- Plot1 + facet_wrap(~accident_severity , scales = "free")
-Subplot1
+#Plot1
+#Subplot1 <- Plot1 + facet_wrap(~accident_severity , scales = "free")
+#Subplot1
 
 
 ##For the next plots I might experiment with heat maps and counter plots, else using standard bar plot...
@@ -37,23 +29,24 @@ Subplot1
 BARPLOT <- function(dataframe, var1, var2){
   dataframe %>%
     ggplot2::ggplot()+
-    ggplot2::geom_bar(mapping = aes(x = {{var1}}, fill = {{var2}}), position = "dodge")+
-   facet_wrap(vars({{var2}}), scales = "free")
+      ggplot2::geom_bar(mapping = aes(x = {{var1}}, fill = {{var2}}), position = "dodge")+
+        ggplot2::facet_wrap(vars({{var2}}), scales = "free")
 }
 
 # BARPLOT(accidents21, <var1>, <var2>)
 
 
-BARPLOTSpecial1 <- function(dataframe, var1, var2){
-
-}
-
 PIECHART <- function(dataframe, var1, var2) {
-  ggplot(dataframe, aes(x = {{var1}}, fill = {{var2 }})) +
-    geom_bar( width = 1) +
-    coord_polar(theta = "y") +
-    xlab(NULL) +
-    ylab(NULL)
+  dataframe %>%
+    dplyr::group_by({{var1}}, {{var2}}) %>%
+      dplyr::summarise(cnt = n(), .groups = "drop_last") %>%
+        dplyr::mutate(props1 = cnt/sum(cnt)) %>%
+          ggplot2::ggplot( aes(x = {{var1}}, fill = {{var2 }})) +
+           ggplot2::geom_bar( width = 1) +
+            ggplot2::coord_polar(theta = "y") +
+              ggplot2::xlab(NULL) +
+               ggplot2::ylab(NULL)+
+                  ggplot2::facet_wrap(vars({{var1}}))
 
 }
 # PIECHART(accidents21, <var1>, <var2>)
@@ -69,6 +62,6 @@ COUNTPLOT <- function(dataframe, var1, var2, var3){
 HEATMAPS <- function(dataframe, var1, var2){
   dataframe %>%
     count({{var1}}, {{var2}}) %>%
-  ggplot2::ggplot(aes(x = {{var1}}, {{var2}}, fill = n))+
-    geom_tile()
+      ggplot2::ggplot(aes(x = {{var1}}, {{var2}}, fill = n))+
+        geom_tile()
 }
