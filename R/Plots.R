e99405a0d34db41f3329ea2975c09b238bd77302
Teo Ghidersa <teos.ghidersa@outlook.de>
2023-01-25 15:12:47 +0100

    new code

--- R/Plots.R
+++ R/Plots.R
@@ -21,45 +21,54 @@ accidents21$day_of_week <- factor(
 Plot1 <- ggplot2::ggplot(data = accidents21)+
   ggplot2::geom_bar(mapping = aes(x = day_of_week, fill = accident_severity), position = "dodge") +
   ggplot2::labs(title = "Total amount of traffic accidents in 2021 (UK)",
-                subtitle = "In progress...",
+                subtitle = "Accident severity included on the plot",
                 caption = "published by the government of the United Kingdom")+
-  scale_fill_brewer(name = "Accident Severity", palette = "Set1")+
+  scale_fill_brewer(name = "Accident severity", palette = "Set1")+
   scale_y_continuous(name ="Amount of accidents")+
   scale_x_discrete(name = "Day of week")
 
 Plot1
-
 Subplot1 <- Plot1 + facet_wrap(~accident_severity , scales = "free")
-
 Subplot1
+
 
 ##For the next plots I might experiment with heat maps and counter plots, else using standard bar plot...
 ## I am interested in accident severity
- Plot2 <- ggplot2::ggplot(data = accidents21)+
-   ggplot2::geom_boxplot(mapping = aes(x = day_of_week, y = number_of_vehicles))+
-   ggplot2::labs(title = "",
-                 subtitle ="",
-                 caption = "published by the government of the United Kingdom")
-   ##scale_color_viridis(name = "Number of Vehicles", option = "viridis")
+BARPLOT <- function(dataframe, var1, var2){
+  dataframe %>%
+    ggplot2::ggplot()+
+    ggplot2::geom_bar(mapping = aes(x = {{var1}}, fill = {{var2}}), position = "dodge")+
+   facet_wrap(vars({{var2}}), scales = "free")
+}
 
-Plot2
-
-
-Subplot2 <- Plot2 + facet_wrap(~number_of_casualties, scales = "free_x")
-
-Subplot2
+# BARPLOT(accidents21, <var1>, <var2>)
 
 
-Plot3 <- ggplot2::ggplot((data = count(accidents21,accident_severity, number_of_vehicles )))+
-  ggplot2::geom_tile(mapping = aes(x = accident_severity, y = number_of_vehicles, fill = n))+
+BARPLOTSpecial1 <- function(dataframe, var1, var2){
 
-scale_fill_viridis(name = "Amount", option = "rainbow")
-Plot3
+}
 
-Plot3 <- ggplot2::ggplot((data = count(accidents21,light_conditions, number_of_casualties )))+
-  ggplot2::geom_tile(mapping = aes(x = light_conditions, y = number_of_casualties, fill = n))
+PIECHART <- function(dataframe, var1, var2) {
+  ggplot(dataframe, aes(x = {{var1}}, fill = {{var2 }})) +
+    geom_bar( width = 1) +
+    coord_polar(theta = "y") +
+    xlab(NULL) +
+    ylab(NULL)
 
+}
+# PIECHART(accidents21, <var1>, <var2>)
 
-Plot3
+COUNTPLOT <- function(dataframe, var1, var2, var3){
+ ggplot2::ggplot(dataframe, aes(x = {{var1}}, y = {{var2}}))+
+  ggplot2::geom_count(aes(size = after_stat(prop), group = {{var2}})) +
+    scale_size_area(max_size = 10)
+}
 
+# COUNTPLOT(accidents21, <var1>, <var2>, <var3>)
 
+HEATMAPS <- function(dataframe, var1, var2){
+  dataframe %>%
+    count({{var1}}, {{var2}}) %>%
+  ggplot2::ggplot(aes(x = {{var1}}, {{var2}}, fill = n))+
+    geom_tile()
+}
