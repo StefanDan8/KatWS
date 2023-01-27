66450d9e8edc20965c2d46c2b0ed55b458af07d8
ge47zot <stefan.dan@tum.de>
2023-01-28 00:54:56 +0100

    started documenting the functions

--- R/num_desc.R
+++ R/num_desc.R
@@ -80,28 +80,33 @@ uniques_per_var <- function(dataframe, var1, var2 ){
   n2 <- paste0("values_of_",deparse1(substitute(var2)));
   ans <- dataframe %>%
     dplyr::group_by({{var1}}) %>%
-      dplyr::summarise(!!n2 := list(unique({{var2}}))) %>%
+      dplyr::summarise(!!n2 := list(sort(unique({{var2}})))) %>%
         dplyr::mutate(display = purrr::map_chr(get(n2), toString))
   return(ans)
 }
 
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param dataframe PARAM_DESCRIPTION
-#' @param var1 PARAM_DESCRIPTION
-#' @param var2 PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Cross Tabulation
+#' @description `contingency_table()` builds a contingency table of the counts at
+#' each combination of values from two columns of a data frame.
+#' @param dataframe a data frame or data frame extension (e.g. a tibble)
+#' @param var1 name of column in `dataframe`
+#' @param var2 name of column in `dataframe`
+#' @return `contingency_table()` returns a contingency table represented as
+#' an object of type tibble
+#' @details the first column is given by `var1` and the following columns are
+#' named by the unique values of `var2`. Each cell represents the count of
+#' `var1`-`var2` pairs for the corresponding values.
 #' @examples
+#' contingency_table(accidents21, accident_severity, urban_or_rural_area)
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' # var1 and var2 must be passed as they are, without quotes or apostrophes.
+#' contingency_table(accidents21, "accident_severity", "urban_or_rural_area")
+#' # throws an error.
 #' }
 #' @seealso
-#'  \code{\link[dplyr]{group_by}}, \code{\link[dplyr]{summarise}}, \code{\link[dplyr]{context}}
-#'  \code{\link[tidyr]{spread}}
+#'  \code{\link[katws]{contingency_table_scale}} is a extension of this function
+#'  that gives the percentage the frequency of each pair among all pairs
 #' @rdname contingency_table
 #' @export
 #' @importFrom dplyr group_by summarise n ungroup
@@ -143,15 +148,15 @@ contingency_table <- function(dataframe, var1, var2){
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 contingency_table_scale <- function(dataframe, var1, var2){
-  n2 <- deparse1(substitute(var1))
+  total <- nrow(dataframe);
+  n2 <- deparse1(substitute(var1));
   ans <- dataframe %>%
     dplyr::group_by({{var1}}, {{var2}}) %>%
-      dplyr::summarise(n = n() ,.groups = "drop_last") %>%
-        dplyr::mutate(prop = n/sum(n)) %>%
-          dplyr::select({{var1}}, {{var2}}, .data$prop) %>%
-            tidyr::spread({{var2}}, .data$prop) %>%
-              dplyr::ungroup() %>%
-                tibble::add_column(n = how_many(dataframe, {{var1}})$n, .after = n2)
+      dplyr::summarise(n = n(),.groups = "drop_last") %>%
+        dplyr::mutate(prop = n/total) %>%
+          dplyr::select({{var1}}, {{var2}}, prop) %>%
+            tidyr::spread({{var2}}, prop) %>%
+              dplyr::ungroup()
 
   return(ans)
 }
